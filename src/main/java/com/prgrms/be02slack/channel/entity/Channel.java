577b4650dc5ecf7e7f36d3d7588176e1db733871
hayoung <>
2022-06-22 14:54:12 +0900

    :sparkles: XS2-78 Channel 엔티티 작성 및 클래스 구조 작성

---
+++ src/main/java/com/prgrms/be02slack/channel/entity/Channel.java
@@ -0,0 +1,51 @@
+package com.prgrms.be02slack.channel.entity;
+
+import com.prgrms.be02slack.common.entity.BaseTime;
+import com.prgrms.be02slack.workspace.entity.Workspace;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.Lob;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+public class Channel extends BaseTime {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @NotNull
+  @Size(min = 1, max = 80)
+  private String name;
+
+  @Lob
+  private String description;
+
+  @NotNull
+  private boolean isPrivate;
+
+  protected Channel() {/*no-op*/}
+
+  public Channel(String name, String description, boolean isPrivate,
+      Workspace workspace) {
+    this.name = name;
+    this.description = description;
+    this.isPrivate = isPrivate;
+    this.workspace = workspace;
+  }
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "workspace_id")
+  private Workspace workspace;
+
+  /**
+   소유자와의 연관관계 필요
+   */
+}
