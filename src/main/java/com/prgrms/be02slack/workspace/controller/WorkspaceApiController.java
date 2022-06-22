9703723f92d4cc4481c4387ac2a1e7bac7dccf1d
San Kim <waterfog9580@gmail.com>
2022-06-22 15:42:52 +0900

    :sparkles: WorkspaceApiController update 메서드 구현 및 연관된 구조 작성

--- src/main/java/com/prgrms/be02slack/workspace/controller/WorkspaceApiController.java
+++ src/main/java/com/prgrms/be02slack/workspace/controller/WorkspaceApiController.java
@@ -1,8 +1,14 @@
 package com.prgrms.be02slack.workspace.controller;
 
+import javax.validation.Valid;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.prgrms.be02slack.workspace.controller.dto.WorkspaceUpdateRequest;
 import com.prgrms.be02slack.workspace.service.WorkspaceService;
 
 @RestController
@@ -14,4 +20,14 @@ public class WorkspaceApiController {
   public WorkspaceApiController(WorkspaceService workspaceService) {
     this.workspaceService = workspaceService;
   }
+
+  @PutMapping("{key}")
+  public void update(
+      @PathVariable String key,
+      @Valid @RequestBody WorkspaceUpdateRequest request
+  ) {
+    final var workspace = WorkspaceUpdateRequest.toEntity(request);
+    workspaceService.update(key, workspace);
+  }
+
 }
--- src/main/java/com/prgrms/be02slack/workspace/service/WorkspaceService.java
+++ src/main/java/com/prgrms/be02slack/workspace/service/WorkspaceService.java
@@ -1,5 +1,8 @@
 package com.prgrms.be02slack.workspace.service;
 
+import com.prgrms.be02slack.workspace.entity.Workspace;
+
 public interface WorkspaceService {
 
+  void update(String key, Workspace workspace);
 }
