3f72c0e7af1e76413e07a8e1f43da33a70f8b673
hayoung <>
2022-06-25 00:36:10 +0900

    :sparkles: XS2-126 ChannelApiController - create 메서드 구현

--- src/main/java/com/prgrms/be02slack/channel/service/ChannelService.java
+++ src/main/java/com/prgrms/be02slack/channel/service/ChannelService.java
@@ -1,4 +1,7 @@
 package com.prgrms.be02slack.channel.service;
 
+import com.prgrms.be02slack.channel.controller.dto.ChannelSaveRequest;
+
 public interface ChannelService {
+  String create(ChannelSaveRequest channelSaveRequest);
 }
