c0020dc2fc5567671da4f8ab3409ab9b6ba5c93f
ecvheo1 <ecvheo@naver.com>
2022-06-25 02:48:39 +0900

    :sparkles: 이메일 인증 메일 전송 구현

---
+++ src/main/java/com/prgrms/be02slack/email/repository/EmailRepository.java
@@ -0,0 +1,15 @@
+package com.prgrms.be02slack.email.repository;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class EmailRepository {
+  private final Map<String, String> map = new ConcurrentHashMap<>();
+
+  public void saveCode(String email, String code) {
+    map.put(email, code);
+  }
+}
