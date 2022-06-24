aa0c7a3fdb729aa0ba73dd1ce0f13ec68268892c
UJ15 <dbwogml15@naver.com>
2022-06-24 22:35:46 +0900

    :sparkles: XS2-60 WorkSpace create메서드 Production 코드 구현

--- src/main/java/com/prgrms/be02slack/workspace/service/WorkspaceService.java
+++ src/main/java/com/prgrms/be02slack/workspace/service/WorkspaceService.java
@@ -1,8 +1,12 @@
 package com.prgrms.be02slack.workspace.service;
 
+import org.hibernate.jdbc.Work;
+
 import com.prgrms.be02slack.workspace.entity.Workspace;
 
 public interface WorkspaceService {
+
+  Workspace create();
 
   void update(String key, Workspace updateWorkspace);
 
