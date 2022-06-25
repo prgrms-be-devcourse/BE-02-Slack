9e0d990ccd958900defa9b078229ffc7540c4890
hayoung <>
2022-06-25 16:13:01 +0900

    :sparkles: XS2-128 Channel 생성 서비스, 레포지토리 기능 구현과 Channel 엔티티 메서드 위치 변경

---
+++ src/main/java/com/prgrms/be02slack/channel/repository/ChannelRepository.java
@@ -0,0 +1,8 @@
+package com.prgrms.be02slack.channel.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.prgrms.be02slack.channel.entity.Channel;
+
+public interface ChannelRepository extends JpaRepository<Channel, Long> {
+}
--- src/main/java/com/prgrms/be02slack/channel/service/DefaultChannelService.java
+++ src/main/java/com/prgrms/be02slack/channel/service/DefaultChannelService.java
@@ -1,13 +1,53 @@
 package com.prgrms.be02slack.channel.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
 
 import com.prgrms.be02slack.channel.controller.dto.ChannelSaveRequest;
+import com.prgrms.be02slack.channel.entity.Channel;
+import com.prgrms.be02slack.channel.repository.ChannelRepository;
+import com.prgrms.be02slack.common.exception.NotFoundException;
+import com.prgrms.be02slack.common.util.IdEncoder;
+import com.prgrms.be02slack.workspace.entity.Workspace;
+import com.prgrms.be02slack.workspace.repository.WorkspaceRepository;
 
 @Service
 public class DefaultChannelService implements ChannelService {
+  private final ChannelRepository channelRepository;
+  private final WorkspaceRepository workspaceRepository;
+  private final IdEncoder idEncoder;
+
+  public DefaultChannelService(
+      ChannelRepository channelRepository,
+      WorkspaceRepository workspaceRepository, IdEncoder idEncoder) {
+    this.channelRepository = channelRepository;
+    this.workspaceRepository = workspaceRepository;
+    this.idEncoder = idEncoder;
+  }
+
+  /**
+   * 1. 멤버(소유주) 정보 파라미터로 넘어오면 이후 처리 구현 필요
+   * 2. 테스트 수정 필요
+   */
   @Override
   public String create(ChannelSaveRequest channelSaveRequest) {
-    return null;
+    Assert.notNull(channelSaveRequest, "ChannelSaveRequest must be provided");
+
+    long workspaceId = idEncoder.decode(channelSaveRequest.getWorkspaceId());
+    Workspace workspace = workspaceRepository.findById(workspaceId)
+        .orElseThrow(() -> new NotFoundException("Workspace not found"));
+
+    // 멤버 조회 로직 구현 필요
+
+    Channel channel = Channel.builder()
+        .name(channelSaveRequest.getName())
+        .description(channelSaveRequest.getDescription())
+        .isPrivate(channelSaveRequest.isPrivate())
+        .workspace(workspace)
+        .owner(null) // <- 이 부분 수정 필요
+        .build();
+    Channel savedChannel = channelRepository.save(channel);
+
+    return idEncoder.encode(savedChannel.getId());
   }
 }
