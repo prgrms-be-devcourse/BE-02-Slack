a75d6f14467bd263eb7802f09b3bb52a074ca258
ecvheo1 <ecvheo@naver.com>
2022-06-26 17:39:32 +0900

    :adhesive_bandage: constructBinding으로 수정

--- src/main/java/com/prgrms/be02slack/Application.java
+++ src/main/java/com/prgrms/be02slack/Application.java
@@ -2,9 +2,13 @@ package com.prgrms.be02slack;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import com.prgrms.be02slack.common.configuration.security.JwtConfig;
+
 @EnableJpaAuditing
+@EnableConfigurationProperties(JwtConfig.class)
 @SpringBootApplication
 public class Application {
 
--- src/main/java/com/prgrms/be02slack/common/configuration/security/JwtConfig.java
+++ src/main/java/com/prgrms/be02slack/common/configuration/security/JwtConfig.java
@@ -1,27 +1,24 @@
 package com.prgrms.be02slack.common.configuration.security;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.stereotype.Component;
+import org.springframework.boot.context.properties.ConstructorBinding;
 
-@Component
 @ConfigurationProperties(prefix = "jwt")
+@ConstructorBinding
 public class JwtConfig {
-  private String tokenSecret;
-  private long tokenExpirationMsec;
+  private final String tokenSecret;
+  private final long tokenExpirationMsec;
+
+  public JwtConfig(String tokenSecret, long tokenExpirationMsec) {
+    this.tokenSecret = tokenSecret;
+    this.tokenExpirationMsec = tokenExpirationMsec;
+  }
 
   public String getTokenSecret() {
     return tokenSecret;
   }
 
-  public void setTokenSecret(String tokenSecret) {
-    this.tokenSecret = tokenSecret;
-  }
-
   public long getTokenExpirationMsec() {
     return tokenExpirationMsec;
-  }
-
-  public void setTokenExpirationMsec(long tokenExpirationMsec) {
-    this.tokenExpirationMsec = tokenExpirationMsec;
   }
 }
