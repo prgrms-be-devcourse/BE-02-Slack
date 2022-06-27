b7dd675dd96db103e8d46ed207e291fff453224a
ecvheo1 <ecvheo@naver.com>
2022-06-27 19:14:23 +0900

    :sparkles: 토큰 필터 설정 추가

--- src/main/java/com/prgrms/be02slack/common/configuration/security/SecurityConfig.java
+++ src/main/java/com/prgrms/be02slack/common/configuration/security/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.prgrms.be02slack.common.configuration.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -51,6 +52,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
   @Bean
   public AuthenticationEntryPointImpl authenticationEntryPointImpl() {
     return new AuthenticationEntryPointImpl();
+  }
+
+  @Bean
+  public TokenAuthenticationFilter tokenAuthenticationFilter() {
+    return new TokenAuthenticationFilter(tokenProvider, customUserDetailsService);
+  }
+
+  @Override
+  public void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+    authenticationManagerBuilder.userDetailsService(customUserDetailsService);
   }
 }
 
