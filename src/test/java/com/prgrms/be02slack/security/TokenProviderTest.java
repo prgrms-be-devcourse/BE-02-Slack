10770b748d38cae8010bec91cfd3053c47ada811
ecvheo1 <ecvheo@naver.com>
2022-06-27 19:12:20 +0900

    :white_check_mark: 토큰 발급 테스트

--- src/test/java/com/prgrms/be02slack/security/TokenProviderTest.java
+++ src/test/java/com/prgrms/be02slack/security/TokenProviderTest.java
@@ -11,6 +11,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import com.prgrms.be02slack.common.configuration.security.JwtConfig;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 
 @ExtendWith(MockitoExtension.class)
@@ -23,20 +24,20 @@ public class TokenProviderTest {
   TokenProvider tokenProvider;
 
   @Test
-  void createTokenTest() {
+  void createLoginTokenTest() {
     //given
     final String email = "test@test.com";
     given(jwtConfig.getTokenSecret()).willReturn("testTokenSecretKey");
     given(jwtConfig.getTokenExpirationMsec()).willReturn(1000000L);
 
     //when, then
-    String createdToken = tokenProvider.createToken(email);
-    String subject = Jwts.parser()
+    String createdToken = tokenProvider.createLoginToken(email);
+    Claims claims = Jwts.parser()
         .setSigningKey(jwtConfig.getTokenSecret())
         .parseClaimsJws(createdToken)
-        .getBody()
-        .getSubject();
+        .getBody();
 
-    assertThat(subject).isEqualTo(email);
+    assertThat(claims.getSubject()).isEqualTo(email);
+    assertThat(claims.get("type")).isEqualTo("Login");
   }
 }
