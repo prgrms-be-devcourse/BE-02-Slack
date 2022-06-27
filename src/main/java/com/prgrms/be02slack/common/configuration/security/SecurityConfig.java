8e1648432a1f1ee340d2d5ea033994e86b43fe1e
ecvheo1 <ecvheo@naver.com>
2022-06-27 19:17:56 +0900

    :sparkles: 토큰 필터 설정 추가

--- src/main/java/com/prgrms/be02slack/common/configuration/security/SecurityConfig.java
+++ src/main/java/com/prgrms/be02slack/common/configuration/security/SecurityConfig.java
@@ -7,13 +7,25 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import com.prgrms.be02slack.security.AccessDeniedHandlerImpl;
 import com.prgrms.be02slack.security.AuthenticationEntryPointImpl;
+import com.prgrms.be02slack.security.CustomUserDetailsService;
+import com.prgrms.be02slack.security.TokenAuthenticationFilter;
+import com.prgrms.be02slack.security.TokenProvider;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+  private final TokenProvider tokenProvider;
+  private final CustomUserDetailsService customUserDetailsService;
+
+  public SecurityConfig(TokenProvider tokenProvider, CustomUserDetailsService customUserDetailsService) {
+    this.tokenProvider = tokenProvider;
+    this.customUserDetailsService = customUserDetailsService;
+  }
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
@@ -41,7 +53,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
           .and()
         .authorizeRequests()
           .anyRequest().permitAll()
-          .and();
+          .and()
+        .addFilterBefore(tokenAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
   }
 
   @Bean
