80ace9b5821a8594e21820e2ea0918d53a222fc2
UJ15 <dbwogml15@naver.com>
2022-06-28 23:53:39 +0900

    :adhesive_bandage: XS2-104 Order 어노테이션으로 순서 보장

--- src/main/java/com/prgrms/be02slack/common/exception/ExceptionController.java
+++ src/main/java/com/prgrms/be02slack/common/exception/ExceptionController.java
@@ -4,6 +4,7 @@ import javax.validation.ConstraintViolationException;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -19,18 +20,21 @@ public class ExceptionController {
   private final Logger logger = LoggerFactory.getLogger(this.getClass());
   @ExceptionHandler(MethodArgumentNotValidException.class)
   @ResponseStatus(HttpStatus.BAD_REQUEST)
+  @Order(1)
   public void handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
     logger.warn("MethodArgumentNotValidException : ", e);
   }
 
   @ExceptionHandler(MethodArgumentTypeMismatchException.class)
   @ResponseStatus(HttpStatus.BAD_REQUEST)
+  @Order(2)
   public void handleMethodArgumentTypeMismatchException(MethodArgumentTypeMismatchException e) {
     logger.warn("MethodArgumentTypeMismatchException : ", e);
   }
 
   @ExceptionHandler(HttpMessageNotReadableException.class)
   @ResponseStatus(HttpStatus.BAD_REQUEST)
+  @Order(3)
   public void handleHttpMessageNotReadableException(HttpMessageNotReadableException e) {
     logger.warn("HttpMessageNotReadableException : ", e);
   }
@@ -39,24 +43,28 @@ public class ExceptionController {
       ConstraintViolationException.class,
       MissingPathVariableException.class})
   @ResponseStatus(HttpStatus.BAD_REQUEST)
+  @Order(4)
   public void handleConstraintViolationException(Exception e) {
     logger.warn("PathVariable is blank : ", e);
   }
 
   @ExceptionHandler(NotFoundException.class)
   @ResponseStatus(HttpStatus.NOT_FOUND)
+  @Order(5)
   public void handleNotFoundException(NotFoundException e) {
     logger.warn("NotFoundException : ", e);
   }
 
   @ExceptionHandler(RuntimeException.class)
   @ResponseStatus(HttpStatus.BAD_REQUEST)
+  @Order(6)
   public void handleRuntimeException(RuntimeException e) {
     logger.error("RuntimeException : ", e);
   }
 
   @ExceptionHandler(Exception.class)
   @ResponseStatus(HttpStatus.BAD_REQUEST)
+  @Order(7)
   public void handleException(Exception e) {
     logger.error("Exception :", e);
   }
