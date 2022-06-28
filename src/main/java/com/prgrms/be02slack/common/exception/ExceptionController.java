87bae126f632e3c7172991b5fde35214c4d26e96
UJ15 <dbwogml15@naver.com>
2022-06-28 23:43:03 +0900

    :sparkles: XS2-104 errorhandler, logger 추가

--- src/main/java/com/prgrms/be02slack/common/exception/ExceptionController.java
+++ src/main/java/com/prgrms/be02slack/common/exception/ExceptionController.java
@@ -2,19 +2,62 @@ package com.prgrms.be02slack.common.exception;
 
 import javax.validation.ConstraintViolationException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.MissingPathVariableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 @ControllerAdvice
 public class ExceptionController {
+
+  private final Logger logger = LoggerFactory.getLogger(this.getClass());
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  @ResponseStatus(HttpStatus.BAD_REQUEST)
+  public void handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+    logger.warn("MethodArgumentNotValidException : ", e);
+  }
+
+  @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+  @ResponseStatus(HttpStatus.BAD_REQUEST)
+  public void handleMethodArgumentTypeMismatchException(MethodArgumentTypeMismatchException e) {
+    logger.warn("MethodArgumentTypeMismatchException : ", e);
+  }
+
+  @ExceptionHandler(HttpMessageNotReadableException.class)
+  @ResponseStatus(HttpStatus.BAD_REQUEST)
+  public void handleHttpMessageNotReadableException(HttpMessageNotReadableException e) {
+    logger.warn("HttpMessageNotReadableException : ", e);
+  }
 
   @ExceptionHandler(value = {
       ConstraintViolationException.class,
       MissingPathVariableException.class})
   @ResponseStatus(HttpStatus.BAD_REQUEST)
   public void handleConstraintViolationException(Exception e) {
+    logger.warn("PathVariable is blank : ", e);
+  }
+
+  @ExceptionHandler(NotFoundException.class)
+  @ResponseStatus(HttpStatus.NOT_FOUND)
+  public void handleNotFoundException(NotFoundException e) {
+    logger.warn("NotFoundException : ", e);
+  }
+
+  @ExceptionHandler(RuntimeException.class)
+  @ResponseStatus(HttpStatus.BAD_REQUEST)
+  public void handleRuntimeException(RuntimeException e) {
+    logger.error("RuntimeException : ", e);
+  }
+
+  @ExceptionHandler(Exception.class)
+  @ResponseStatus(HttpStatus.BAD_REQUEST)
+  public void handleException(Exception e) {
+    logger.error("Exception :", e);
   }
 }
