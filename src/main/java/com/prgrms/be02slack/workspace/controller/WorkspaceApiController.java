7d21de29257eda5d19c60f45d5fc5e4f4417eefb
dojinyou <djyou128@gmail.com>
2022-06-29 15:58:59 +0900

    :recycle: WorkspaceApiController 함수 구조 리팩토링
    ResponseStatus 추가하여 형식 통일
    argument의 Valid 어노테이션을 class의 Validated로 일괄 적용

--- src/main/java/com/prgrms/be02slack/workspace/controller/WorkspaceApiController.java
+++ src/main/java/com/prgrms/be02slack/workspace/controller/WorkspaceApiController.java
@@ -1,11 +1,12 @@
 package com.prgrms.be02slack.workspace.controller;
 
-import javax.validation.Valid;
-
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.prgrms.be02slack.workspace.controller.dto.WorkspaceUpdateRequest;
@@ -13,6 +14,7 @@ import com.prgrms.be02slack.workspace.service.WorkspaceService;
 
 @RestController
 @RequestMapping("api/v1/workspaces")
+@Validated
 public class WorkspaceApiController {
 
   private final WorkspaceService workspaceService;
@@ -22,7 +24,8 @@ public class WorkspaceApiController {
   }
 
   @PutMapping("{key}")
-  public void update(@PathVariable String key, @Valid @RequestBody WorkspaceUpdateRequest request) {
+  @ResponseStatus(HttpStatus.OK)
+  public void update(@PathVariable String key, @RequestBody WorkspaceUpdateRequest request) {
     final var workspace = WorkspaceUpdateRequest.toEntity(request);
     workspaceService.update(key, workspace);
   }
