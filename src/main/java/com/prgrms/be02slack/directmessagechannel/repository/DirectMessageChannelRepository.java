4ade18d1e4187af5aa2c4fe941bbbaaaa8720e16
UJ15 <dbwogml15@naver.com>
2022-07-03 00:21:37 +0900

    :sparkles: XS2-73 DMchannel repository 작성

---
+++ src/main/java/com/prgrms/be02slack/directmessagechannel/repository/DirectMessageChannelRepository.java
@@ -0,0 +1,13 @@
+package com.prgrms.be02slack.directmessagechannel.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.prgrms.be02slack.directmessagechannel.entity.DirectMessageChannel;
+import com.prgrms.be02slack.member.entity.Member;
+
+public interface DirectMessageChannelRepository extends JpaRepository<DirectMessageChannel, Long> {
+
+  Optional<DirectMessageChannel> findByFirstMemberAndSecondMember(Member firstMember, Member secondMember);
+}
