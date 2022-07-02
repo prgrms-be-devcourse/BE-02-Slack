5fc0a6b5d3dac997342c73d9a20586a6b04e81e6
UJ15 <dbwogml15@naver.com>
2022-07-03 03:00:11 +0900

    :white_check_mark: XS2-73 Service Test 통과

--- src/test/java/com/prgrms/be02slack/directmessagechannel/service/DirectMessageChannelServiceTest.java
+++ src/test/java/com/prgrms/be02slack/directmessagechannel/service/DirectMessageChannelServiceTest.java
@@ -1,6 +1,5 @@
 package com.prgrms.be02slack.directmessagechannel.service;
 
-
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
@@ -12,20 +11,28 @@ import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 
+import org.mockito.MockedConstruction;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.test.util.ReflectionTestUtils;
 
 import com.prgrms.be02slack.common.exception.NotFoundException;
+import com.prgrms.be02slack.common.util.IdEncoder;
+import com.prgrms.be02slack.directmessagechannel.entity.DirectMessageChannel;
 import com.prgrms.be02slack.directmessagechannel.repository.DirectMessageChannelRepository;
 import com.prgrms.be02slack.member.entity.Member;
 import com.prgrms.be02slack.member.service.MemberService;
 import com.prgrms.be02slack.util.WithMockCustomLoginUser;
+import com.prgrms.be02slack.workspace.entity.Workspace;
 import com.prgrms.be02slack.workspace.service.WorkspaceService;
 
 import static org.mockito.ArgumentMatchers.*;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
+
+import java.util.Optional;
 
 @ExtendWith(MockitoExtension.class)
 public class DirectMessageChannelServiceTest {
@@ -40,10 +47,13 @@ public class DirectMessageChannelServiceTest {
   private MemberService memberService;
 
   @Mock
-  SecurityContext securityContextMocked;
+  private IdEncoder idEncoder;
 
   @Mock
-  Authentication authenticationMocked;
+  private SecurityContext securityContextMocked;
+
+  @Mock
+  private Authentication authenticationMocked;
 
   @InjectMocks
   DefaultDirectMessageChannelService directMessageChannelService;
@@ -72,7 +82,7 @@ public class DirectMessageChannelServiceTest {
 
         //then
         Assertions.assertThatThrownBy(
-            () -> directMessageChannelService.create(workspaceId, validReceiverEmail))
+                () -> directMessageChannelService.create(workspaceId, validReceiverEmail))
             .isInstanceOf(IllegalArgumentException.class);
       }
     }
@@ -125,12 +135,15 @@ public class DirectMessageChannelServiceTest {
         //given
         final String validWorkspaceId = "testId";
         final String notExistReceiverEmail = "test@test.test";
+        final Workspace workspace = new Workspace("test", "test");
+
+        when(workspaceService.findByKey(any())).thenReturn(workspace);
 
         when(authenticationMocked.getName()).thenReturn("test");
         when(securityContextMocked.getAuthentication()).thenReturn(authenticationMocked);
         SecurityContextHolder.setContext(securityContextMocked);
 
-        when(memberService.findByEmailAndWorkspaceKey(any(),any()))
+        when(memberService.findByEmailAndWorkspaceKey(any(), any()))
             .thenThrow(NotFoundException.class);
 
         //then
@@ -141,28 +154,114 @@ public class DirectMessageChannelServiceTest {
     }
 
     @Nested
-    @DisplayName("유효한 인자를 받으면")
-    class ContextValidArgument {
+    @DisplayName("유효한 인자를 받았을때 해당 dm 채널이 존재하면")
+    class ContextValidArgumentAndeExistChannel {
 
       @Test
-      @DisplayName("채널 Id를 반환한다.")
+      @DisplayName("해당 채널 Id를 반환한다.")
       void itReturnDirectMessageChannelId() {
+
         //given
         final String validWorkspaceId = "testId";
-        final String notExistReceiverEmail = "test@test.test";
+        final String validReceiverEmail = "test@test.test";
+        final Workspace workspace =
+            new Workspace(
+                "test",
+                "test"
+            );
+        final DirectMessageChannel testDirectMessageChannel =
+            new DirectMessageChannel(
+                testMember,
+                testMember,
+                workspace
+            );
+
+        ReflectionTestUtils.setField(testDirectMessageChannel, "id", 1L);
+
+        when(workspaceService.findByKey(any())).thenReturn(workspace);
 
         when(authenticationMocked.getName()).thenReturn("test");
         when(securityContextMocked.getAuthentication()).thenReturn(authenticationMocked);
         SecurityContextHolder.setContext(securityContextMocked);
 
-        //when()
+        when(memberService.findByEmailAndWorkspaceKey(any(), any()))
+            .thenReturn(testMember);
 
-        when(memberService.findByEmailAndWorkspaceKey(any(),any()))
-            .thenThrow(NotFoundException.class);
+        when(directMessageChannelRepository
+            .findByFirstMemberAndSecondMember(testMember, testMember))
+            .thenReturn(Optional.of(testDirectMessageChannel));
+
+        when(idEncoder.encode(testDirectMessageChannel.getId())).thenReturn("testtest");
 
         //when
+        final String actualId =
+            directMessageChannelService.create(validWorkspaceId, validReceiverEmail);
+
+        final String expectedId = "testtest";
 
         //then
+        Assertions.assertThat(expectedId).isEqualTo(actualId);
+
+        verify(workspaceService).findByKey(any());
+        verify(memberService, times(2))
+            .findByEmailAndWorkspaceKey(any(), any());
+        verify(directMessageChannelRepository, times(1))
+            .findByFirstMemberAndSecondMember(any(), any());
+        verify(idEncoder).encode(anyLong());
+      }
+    }
+
+    @Nested
+    @DisplayName("유효한 인자를 받았을때 해당 dm 채널이 존재하지 않으면")
+    class ContextValidArgumentAndNotExistChannel {
+
+      @Test
+      @DisplayName("새로운 채널을 생성하고 Id를 반환한다.")
+      void itReturnDirectMessageChannelId() {
+
+        //given
+        final String validWorkspaceId = "testId";
+        final String validReceiverEmail = "test@test.test";
+        final Workspace workspace =
+            new Workspace(
+                "test",
+                "test"
+            );
+
+        when(workspaceService.findByKey(any())).thenReturn(workspace);
+
+        when(authenticationMocked.getName()).thenReturn("test");
+        when(securityContextMocked.getAuthentication()).thenReturn(authenticationMocked);
+        SecurityContextHolder.setContext(securityContextMocked);
+
+        when(memberService.findByEmailAndWorkspaceKey(any(), any()))
+            .thenReturn(testMember);
+
+        when(directMessageChannelRepository
+            .findByFirstMemberAndSecondMember(testMember, testMember))
+            .thenReturn(Optional.empty());
+
+        MockedConstruction<DirectMessageChannel> mockedConstruction2 =
+            Mockito.mockConstruction(DirectMessageChannel.class,
+                (mock, context) -> when(mock.getId()).thenReturn(1L));
+
+        when(idEncoder.encode(anyLong())).thenReturn("testtest");
+
+        //when
+        final String actualId =
+            directMessageChannelService.create(validWorkspaceId, validReceiverEmail);
+
+        final String expectedId = "testtest";
+
+        //then
+        Assertions.assertThat(expectedId).isEqualTo(actualId);
+
+        verify(workspaceService).findByKey(any());
+        verify(memberService, times(2))
+            .findByEmailAndWorkspaceKey(any(), any());
+        verify(directMessageChannelRepository, times(2))
+            .findByFirstMemberAndSecondMember(any(), any());
+        verify(idEncoder).encode(anyLong());
       }
     }
   }
