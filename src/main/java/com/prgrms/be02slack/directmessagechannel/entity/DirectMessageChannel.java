88669b57d63aedbc7c61fbf3c530ffa41c264ff6
UJ15 <dbwogml15@naver.com>
2022-07-03 00:21:19 +0900

    :sparkles: XS2-73 DMchannel entity 작성

---
+++ src/main/java/com/prgrms/be02slack/directmessagechannel/entity/DirectMessageChannel.java
@@ -0,0 +1,45 @@
+package com.prgrms.be02slack.directmessagechannel.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import com.prgrms.be02slack.common.entity.BaseTime;
+import com.prgrms.be02slack.member.entity.Member;
+import com.prgrms.be02slack.workspace.entity.Workspace;
+
+@Entity
+public class DirectMessageChannel extends BaseTime {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "member_id")
+  private Member firstMember;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "member_id")
+  private Member secondMember;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "workspace_id")
+  private Workspace workspace;
+
+  protected DirectMessageChannel() {}
+
+  public DirectMessageChannel(Member firstMember, Member secondMember, Workspace workspace) {
+    this.firstMember = firstMember;
+    this.secondMember = secondMember;
+    this.workspace = workspace;
+  }
+
+  public Long getId() {
+    return id;
+  }
+}
